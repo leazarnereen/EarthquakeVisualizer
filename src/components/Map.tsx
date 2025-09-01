0cf3451765614f3ba2deed12729e3ddf51193e40
leazarnereen <le123ar456@gmail.com>
2025-09-02 01:35:05 +0530

    Made Changes in CSS

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -95,7 +95,7 @@ const Map = () => {
         ) : error ? (
           <div className="center-message error">{error}</div>
         ) : (
-          <MapContainer center={[20, 0]} zoom={2} style={{ height: "100vh", width: "100%" }}>
+          <MapContainer center={[20, 0]} zoom={2} style={{ height: "100%", width: "100%" }}>
   <TileLayer
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
